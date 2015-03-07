1a9ded12fe3eefb0594d7efa955930655a266a3d
lzbaer <lzbaer@asu.edu>
2015-03-07 14:05:39 -0700

    Teacher and Student login verification

--- authentication.js
+++ authentication.js
@@ -4,23 +4,39 @@ var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 var db = require('./db');
 var bcrypt = require('bcryptjs');
+var isTeacher = true;
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    db.models.User.findOne({ username: username }, function(err, user) {
-      console.log(user);
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      bcrypt.compare(password, user.password, function(err, isMatch) {
+function checkpassword(password, user, done) {
+	 bcrypt.compare(password, user.password, function(err, isMatch) {
           if (isMatch === true && !err) {
-            return done(null, user);
+            return done(null, teacher);
           } else {
             return done(null, false, { message: 'Incorrect password.' });
           }
       });
+}
+
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    db.models.teacher.findOne({ username: username }, function(err, teacher) {
+      	console.log(teacher);
+      	if (err) { return done(err); }
+      	if (!teacher) {
+    		isTeacher = false;
+      		db.models.student.findOne({username: username }, function(err, student) {
+      			console.log(student);
+      			if (err) { return done(err); }
+      			if(!student) {
+      				return done(null, false, { message: 'Incorrect username.' });
+      			}
+      			else {
+      				checkpassword(password, student, done);
+      			}
+      		});
+      	}
+      	else {
+      		checkpassword(password, teacher, done);
+      	}
     });
-  }
-));
+    }));
 
