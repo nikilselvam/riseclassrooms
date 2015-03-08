c39fbe28d290e64839d3ab63f80eb98fb80af436
kevinliao95 <kliao8@asu.edu>
2015-03-07 18:36:59 -0700

    clean up for session.js

--- routes/session.js
+++ routes/session.js
@@ -1,28 +1,23 @@
 var db = require('./../db.js');
 var resError = require('./messaging').resError;
 var Session = db.models.Session;
-var express = require('express'), app = express();
 
 exports.create = function(req, res) {
 	//Return error if req object does not specify startTime,
 	// active, duration, and classID
-	console.log(req.classId);
 	/*
 	if (!req.classId) {
 		return resError(res, "Sorry, this session does not have a specified class.");
 	}*/
 
 	var start = Date.now();
-	console.log(start);
 	var duration = durationToMs(req.body.duration);
-	console.log(duration);
 	var end = start + duration;
-	console.log(end);
 
 	//Create a new session object with the above parameters specified and endTime calculated
 	var sessionObject = new Session({
 		active: true,
-		//classId: req.classId,
+		classId: req.classId,
 		startTime: start,
 		duration: duration,
 		endTime: end
@@ -32,7 +27,6 @@ exports.create = function(req, res) {
 	sessionObject.save(function(err, sessionObject){
 		if (err) return console.error(err);
 
-		console.log(sessionObject);
 		Class.findOne( {_id: req.classId}), function (err, classObject) {
 			if (err || !classId) {
 				return console.error(err);
