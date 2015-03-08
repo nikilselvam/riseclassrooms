3627e324797ec12a984f7ba232d51d48d299a50d
shantanubala <shantanubala@gmail.com>
2015-03-07 17:11:55 -0700

    Added bcrypt password encryption to signup

--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,7 @@
 var db = require('./../db.js');
 var resError = require('./messaging').resError;
 var Student = db.models.Student;
-
+var bcrypt = require('bcryptjs');
 
 exports.create = function(req, res){
 	//console.log(req.body.firstName);
@@ -20,15 +20,20 @@ exports.create = function(req, res){
 		return resError(res, "Please include your last name");
 	}
 
-	var student1 = new Student({
-		name: req.body.email,
-		password: req.body.password,
-		firstName: req.body.firstName,
-		lastName: req.body.lastName
-	});;
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(req.body.password, salt, function(err, hash) {
+			var student1 = new Student({
+				name: req.body.email,
+				password: hash,
+				firstName: req.body.firstName,
+				lastName: req.body.lastName
+			});
 
-	student1.save(function(err, student1) {
-		if (err) return console.error(err);
-		console.log(student1);
+			student1.save(function(err, student1) {
+				if (err) return console.error(err);
+				console.log(student1);
+			});
+		});
 	});
+
 };
--- routes/teacher.js
+++ routes/teacher.js
@@ -3,9 +3,9 @@
 var db = require('./../db.js');
 var resError = require('./messaging').resError;
 var Teacher = db.models.Teacher;
+var bcrypt = require('bcryptjs');
 
 exports.create = function(req, res){
-
 	if(!req.body.email) {
 		return resError(res, "Please include an email address");
 	}
@@ -19,18 +19,20 @@ exports.create = function(req, res){
 		return resError(res, "Please include your last name");
 	}
 
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(req.body.password, salt, function(err, hash) {
+			var teacher1 = new Teacher({
+				email: req.body.email,
+				password: hash,
+				firstName: req.body.firstName,
+				lastName: req.body.lastName
+			});
 
-	var teacher1 = new Teacher({
-		email: req.body.email,
-		password: req.body.password,
-		firstName: req.body.firstName,
-		lastName: req.body.lastName
-	});;
+			teacher1.save(function(err, teacher1) {
+				if (err) return console.error(err);
 
-
-	teacher1.save(function(err, teacher1) {
-		if (err) return console.error(err);
-
-		console.log(teacher1);
+				console.log(teacher1);
+			});
+		});
 	});
 };
